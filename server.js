1d32b02d178c19161cde94522202cb7e96eaec68
Kauan <raidoka546@gmail.com>
2024-08-20 17:18:25 -0400

    changed order of execution

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // npm run dev
 import express from 'express';
-import { router } from './routes/Users.js';
+import privateRoutes from './routes/private/Users.js'
+import publicRoutes from './routes/public/Users.js'
 
 export const app = express();
 const port = 3333;
@@ -8,8 +9,9 @@ const port = 3333;
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
+app.use("/", privateRoutes)
+app.use("/", publicRoutes)
+
 app.listen(port, () => {
     console.log(`Server at localhost:${port}`)
 });
-
-app.use("/", router)
