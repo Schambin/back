bb2884183cda9115d70508770a714a25aa4ebec1
Kauan <raidoka546@gmail.com>
2024-08-31 08:20:14 -0400

    installed cors

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ import express from 'express';
 import privateRoutes from './routes/private/Users.js';
 import publicRoutes from './routes/public/Users.js';
 import { auth } from './middlewares/Auth.js';
+import cors from 'cors';
 
 export const app = express();
 const port = 3333;
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
+app.use(cors('http://localhost:5173', 'https://localhost:5173/register'));
 
 app.use("/p", auth, privateRoutes);
 app.use("/", publicRoutes);
