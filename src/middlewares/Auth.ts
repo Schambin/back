06d05c43e6dc5e68217cdccc4527356bab37c5bf
Kauan <raidoka546@gmail.com>
2024-09-18 09:24:00 -0400

    changing project to typescript

--- src/middlewares/Auth.ts
+++ src/middlewares/Auth.ts
@@ -1,8 +1,16 @@
+import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
-const JWTSecrect = process.env.JWT_SECRET_KEY;
+const JWTSecrect = process.env.JWT_SECRET_KEY as string;
 
-export const auth = (req, res, next) => {
+interface DecodedToken {
+    id: string;
+    name: string;
+    exp: number;
+    iat: number;
+}
+
+export const auth = (req: Request, res: Response, next: NextFunction) => {
 
     const token = req.headers.authorization;
 
@@ -11,7 +19,7 @@ export const auth = (req, res, next) => {
     }
 
     try {
-        const decoded = jwt.verify(token.replace('Bearer ', ''), JWTSecrect);
+        const decoded = jwt.verify(token.replace('Bearer ', ''), JWTSecrect) as  DecodedToken;
         req.user = decoded;
 
         next();
